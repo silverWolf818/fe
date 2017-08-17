20576674f21892175ae89e107fb87ec88ce7ca54
Zhou <363420113@qq.com>
2017-08-17 21:25:45 +0800

    更新

--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -1,8 +1,23 @@
 var path = require("path")
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-     entry:'./src/script/main.js',
+     entry:{
+         // bundle:['./src/script/main.js','./src/script/a.js']
+         main:'./src/script/main.js',
+         a:'./src/script/a.js'
+     },
      output:{
-         path:path.resolve(__dirname, './dist/js'),
-         filename:'bundle.js'
-     }
-}
+         path:path.resolve(__dirname, './dist'),
+         filename:'js/[name]-[hash].js'
+         //filename:'[name]-[chunkhash].js'
+     },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename:'index-[hash].html',
+            template:'index.html',
+            inject:'body',//插入的位置
+            title:'webpack good'
+        })
+    ]
+};
