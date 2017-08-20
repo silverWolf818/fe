73e9494b292ea57aed03863a75d0b77f54426e33
周志豪 <363420113@qq.com>
2017-08-20 22:46:31 +0800

    删除

--- webpack-test./webpack.config.js
+++ webpack-test./webpack.config.js
@@ -4,20 +4,53 @@ var htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
      entry:{
          // bundle:['./src/script/main.js','./src/script/a.js']
-         main:'./src/script/main.js',
-         a:'./src/script/a.js'
+        //  main:'./src/script/main.js',
+         a:'./src/script/a.js',
+         b:'./src/script/b.js',
+         c:'./src/script/c.js'
      },
      output:{
          path:path.resolve(__dirname, './dist'),
-         filename:'js/[name]-[hash].js'
+         filename:'js/[name].js',
+        //  publicPath:'http://cdn.com/'
          //filename:'[name]-[chunkhash].js'
      },
     plugins: [
         new htmlWebpackPlugin({
-            filename:'index-[hash].html',
-            template:'index.html',
+            filename:'a.html',
+            template:'template.html',
             inject:'body',//插入的位置
-            title:'webpack good'
+            title:'a',
+            chunks: ['a'],
+            // date:new Date(),
+            minify:{
+                removeComments:true,
+                //collapseWhitespace:true
+            }
+        }),
+        new htmlWebpackPlugin({
+            filename:'b.html',
+            template:'template.html',
+            inject:'body',//插入的位置
+            title:'b',
+            chunks: ['b'],
+            // date:new Date(),
+            minify:{
+                removeComments:true,
+                //collapseWhitespace:true
+            }
+        }),
+        new htmlWebpackPlugin({
+            filename:'c.html',
+            template:'template.html',
+            inject:'body',//插入的位置
+            title:'c',
+            chunks: ['c'],
+            // date:new Date(),
+            minify:{
+                removeComments:true,
+                //collapseWhitespace:true
+            }
         })
     ]
 };
