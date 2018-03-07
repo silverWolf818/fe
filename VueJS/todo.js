51b57e4e2d4d0d3e5ddfed394cb4d07f4631a692
zhouzh <363420113@qq.com>
2018-03-07 23:49:53 +0800

    完善todo

--- VueJS/todo.js
+++ VueJS/todo.js
@@ -13,7 +13,23 @@ const vm = new Vue({
         todos:[
             { isSelected:false,title:'睡觉' },
             { isSelected:false,title:'吃饭' }
-        ]
+        ],
+        hash:''
+    },
+    created(){
+        this.todos = JSON.parse(localStorage.getItem('data')) || this.todos;
+        this.hash = window.location.hash.slice(2) || 'all';
+        window.addEventListener('hashchange',()=>{
+            this.hash = window.location.hash.slice(2);
+        },false);
+    },
+    watch:{
+      todos:{
+          handler(){
+             localStorage.setItem('data',JSON.stringify(this.todos));
+          },
+          deep:true
+      }
     },
     methods:{
         add(){
@@ -37,6 +53,12 @@ const vm = new Vue({
     computed:{
         count(){
             return this.todos.filter(item=>!item.isSelected).length;
+        },
+        filterTodos() {
+            if (this.hash === 'all') return this.todos;
+            if (this.hash === 'finish') return this.todos.filter(item=>item.isSelected);
+            if (this.hash === 'unfinish') return this.todos.filter(item=>!item.isSelected);
+            return this.todos;
         }
     }
 });
